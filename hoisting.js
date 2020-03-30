443e1fc51b977e09092cb634ebe469152c700c9f
Timothy Johnson <tjohnson@2ndswing.com>
2020-03-30 19:35:55 -0400

    add hoisting study

---
+++ hoisting.js
@@ -0,0 +1,10 @@
+// console.log(dog)
+console.log(cat)
+console.log(dogsAndCats())
+
+
+const dog = 'bow wow' // not hoisted
+var cat = "meow" // partially hoisted
+function dogsAndCats(){
+    return "woof, purr"
+}
